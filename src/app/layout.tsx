aaa5939af05ab5e379e6eb6e0fe2b4edc5b0ed6d
Ayushkalathiya <ayushkalathiya50@gmail.com>
2025-03-21 19:39:50 +0530

    Fix NextIntlClientProvider wrapper and add console logs for locale debugging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <NextIntlClientProviderwrapper messages={{messages}}>
+          <NextIntlClientProviderwrapper messages={messages}>
             {children}
           </NextIntlClientProviderwrapper>
         </ThemeProvider>
