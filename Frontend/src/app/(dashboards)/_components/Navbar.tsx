bb1c0234b1902b3020deb015d4bf9c16c5e3641a
sarthak mayani <mayanisarthak@gmail.com>
2025-03-22 14:40:51 +0530

    USER REDIRECT

--- Frontend/src/app/(dashboards)/_components/Navbar.tsx
+++ Frontend/src/app/(dashboards)/_components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar({children}:{children:React.ReactNode}) {
       className="fixed top-0 z-[100] w-full border-b bg-background/95 backdrop-blur-sm supports-[backdrop-filter]:bg-background/60"
     >
       <div className="container flex h-16 items-center justify-between">
-        <motion.div className="flex items-center gap-2" whileHover={{ scale: 1.05 }}>
+        <motion.div className="flex items-center gap-2" whileHover={{ scale: 1.05 }} onClick={() => window.location.href = "/" }>
           <Image alt="logo" src="/logo/creditunify-logo.svg" width={35} height={35} />
           <span className="text-xl font-bold bg-gradient-to-r from-primary to-blue-400 bg-clip-text text-transparent">
             {t('header.logo')}
