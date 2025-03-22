b927e8c69c3ee09c3a782a46b95468d9af1f0433
Dhruv Kotadiya <22ce054@charusat.edu.in>
2025-03-22 11:57:15 +0530

    Improved dark mode compatibility and made build

--- Frontend/src/auth.ts
+++ Frontend/src/auth.ts
@@ -1,31 +1,35 @@
-import NextAuth ,{type DefaultSession} from "next-auth"
-import authConfig from "./auth.config"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { db } from "./lib/db";
-import { getUserById } from "./data/user";
-import { getTwoFactorConformationByUserId } from "./data/two-factor-conformation";
-import { getAccountByUserId } from "./data/account";
-import { NextResponse } from "next/server";
- 
-export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
-  events:{
-    async linkAccount({user}){
+import NextAuth, { type DefaultSession } from 'next-auth';
+import authConfig from './auth.config';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { db } from './lib/db';
+import { getUserById } from './data/user';
+import { getTwoFactorConformationByUserId } from './data/two-factor-conformation';
+import { getAccountByUserId } from './data/account';
+import { NextResponse } from 'next/server';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  events: {
+    async linkAccount({ user }) {
       await db.user.update({
-        where:{id:user.id},
-        data:{emailVerified:new Date()}
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
       });
     },
   },
-  callbacks:{
+  callbacks: {
     // Modify the jwt token / Action to take while generation of token
-    async jwt({token,user}){
-      // token.sub has user id (USER table id ) 
-      if(!token.sub)
-        return token;
+    async jwt({ token, user }) {
+      // token.sub has user id (USER table id )
+      if (!token.sub) return token;
 
-      const existingUser = await  getUserById(token.sub);
+      const existingUser = await getUserById(token.sub);
 
-      if(!existingUser) return token;
+      if (!existingUser) return token;
 
       const existingAccount = await getAccountByUserId(existingUser.id);
 
@@ -34,37 +38,34 @@ export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
       token.name = existingUser.name;
       token.email = existingUser.email;
       if (user) {
-        token.role = user.role || "USER" // Default to USER if not set
+        token.role = user.role || 'USER'; // Default to USER if not set
       }
-    
+
       token.isTwoFactorEnable = existingUser.isTwoFactorEnable;
-      if(token.role==="USER" ){
-        token.aadhaarNumber = existingUser.aadhaarNumber ?? "";
-        token.PAN = existingUser.PAN ?? "";
+      if (token.role === 'USER') {
+        token.aadhaarNumber = existingUser.aadhaarNumber ?? '';
+        token.PAN = existingUser.PAN ?? '';
       }
       return token;
-
     },
     //Modify the session / Action to take while generation of session
-    async session({token,session}){
+    async session({ token, session }) {
       // console.log({Sessiontoken:token,session});
-      // id of our user is in token.sub 
+      // id of our user is in token.sub
 
-      if(token.sub && session.user) 
-        session.user.id = token.sub;
+      if (token.sub && session.user) session.user.id = token.sub;
 
-      if(token.role && session.user)
-        session.user.role = token.role;
+      if (token.role && session.user) session.user.role = token.role;
 
-      if(session.user){
-        session.user.isTwoFactorEnabled=token.isTwoFactorEnable as boolean;
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnable as boolean;
         session.user.name = token.name as string;
 
-        session.user.email= token.email as string;
+        session.user.email = token.email as string;
         session.user.isOAuth = token.isOAuth as boolean;
       }
 
-      if(session.user.role==="USER"){
+      if (session.user.role === 'USER') {
         session.user.aadhaarNumber = token.aadhaarNumber as string;
         session.user.PAN = token.PAN as string;
       }
@@ -72,13 +73,13 @@ export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
     },
 
     // Action to tack when user signin/login
-    async signIn({user,account}){
-      if (account?.provider === "google") {
+    async signIn({ user, account }) {
+      if (account?.provider === 'google') {
         try {
           const existingUser = await db.user.findUnique({
             // @ts-ignore
             where: { email: user?.email },
-          })
+          });
 
           if (existingUser) {
             // If the user exists but email is not verified, update the emailVerified field
@@ -86,59 +87,60 @@ export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
               await db.user.update({
                 where: { id: existingUser.id },
                 data: { emailVerified: new Date() },
-              })
+              });
             }
-            return true
+            return true;
           } else {
             // If it's a new user, emailVerified will be set automatically by the adapter
-            return true
+            return true;
           }
         } catch (error) {
-          console.error("Error in Google sign in:", error)
-          return false
+          console.error('Error in Google sign in:', error);
+          return false;
         }
       }
 
       try {
-        const existingUser = await getUserById(user.id || "")
+        const existingUser = await getUserById(user.id || '');
 
         // Prevent sign in without email verification
-        if (!existingUser || !existingUser.emailVerified) return false
+        if (!existingUser || !existingUser.emailVerified) return false;
 
         // 2Factor Authentication check
         if (existingUser.isTwoFactorEnable) {
           const twoFactorConfirmation = await getTwoFactorConformationByUserId(
-            existingUser.id
-          )
+            existingUser.id,
+          );
           // console.log("n\n\n\n TFA: ", twoFactorConfirmation)
-          if (!twoFactorConfirmation) return false
+          if (!twoFactorConfirmation) return false;
 
           // Delete two factor confirmation for next sign in
           await db.twoFactorConfirmation.delete({
             where: {
               id: twoFactorConfirmation.id,
             },
-          })
+          });
         }
         const response = NextResponse.next();
-        response.cookies.set("user_role", existingUser.role);
-        console.log("USER LOGED IN:",existingUser);
-        
-        return true
+        response.cookies.set('user_role', existingUser.role);
+        console.log('USER LOGED IN:', existingUser);
+
+        return true;
       } catch (error) {
-        console.error("Error in signIn callback:", error)
-        return false
+        console.error('Error in signIn callback:', error);
+        return false;
       }
     },
   },
+  //@ts-ignore
   adapter: PrismaAdapter(db),
-  session: { strategy: "jwt" },
+  session: { strategy: 'jwt' },
 
   ...authConfig,
-  pages:{
-    signIn:"/auth/login",
+  pages: {
+    signIn: '/auth/login',
 
     // IF any Error comes During OAuth Then Redirect to this page.
-    error:"/auth/error"
-  }
-})
+    error: '/auth/error',
+  },
+});
--- Frontend/src/middleware.ts
+++ Frontend/src/middleware.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth"
-import { NextResponse } from "next/server"
-import authConfig from "./auth.config"
+import NextAuth from 'next-auth';
+import { NextResponse } from 'next/server';
+import authConfig from './auth.config';
 import {
   apiAuthPrefix,
   apiRoutes,
@@ -13,21 +13,22 @@ import {
   privateRoutes,
   privateUSER,
   publicRoutes,
-} from "./routes"
+} from './routes';
 import { cookies } from 'next/headers';
-const { auth } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig);
 
-export default auth((req):any => {
-  const { nextUrl } = req
-  const isLoggedIn = !!req.auth 
-  const isApiAuthRoute = nextUrl.pathname.toString().startsWith(apiAuthPrefix)
-  const isApiRoute = apiRoutes.includes(nextUrl.pathname)
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
-  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname)
-  const isUSER = privateUSER.includes(nextUrl.pathname)
-  const isBANK = privateBANK.includes(nextUrl.pathname)
-  const isADMIN = privateADMIN.includes(nextUrl.pathname)
+export default auth((req): any => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.toString().startsWith(apiAuthPrefix);
+  const isApiRoute = apiRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
+  const isUSER = privateUSER.includes(nextUrl.pathname);
+  //@ts-ignore
+  const isBANK = privateBANK.includes(nextUrl.pathname);
+  const isADMIN = privateADMIN.includes(nextUrl.pathname);
 
   const routeExists =
     isApiAuthRoute ||
@@ -36,59 +37,88 @@ export default auth((req):any => {
     isPrivateRoute ||
     isApiRoute ||
     isUSER ||
-    isADMIN||
-    isBANK
+    isADMIN ||
+    isBANK;
 
-    
-    
-    if (!routeExists) {
-      return NextResponse.redirect(new URL("/404", nextUrl))
-    }
-    
-    if (isApiAuthRoute) {
-      return null
-    }
-    
-    if (isApiRoute && isLoggedIn) {
-      return NextResponse.next()
-    }
-    
-    const role = cookies().get("role")?.value 
-    console.log(nextUrl.pathname,":",isUSER, " ",isADMIN, " ",isBANK," ",req.auth," is login",isLoggedIn,"\nROLE: ",role);
-    
-    if (isAuthRoute || isPublicRoute) {
-      if (isLoggedIn) {
-        let redirecturl=DEFAULT_LOGIN_REDIRECT_USER;
-        if(role==="USER"){
-          redirecturl=DEFAULT_LOGIN_REDIRECT_USER;
-        }else if(role==="ADMIN"){
-          redirecturl=DEFAULT_LOGIN_REDIRECT_ADMIN;
-        }else if(role==="BANK"){
-          redirecturl=DEFAULT_LOGIN_REDIRECT_BANK;
-        }
-        
-        return NextResponse.redirect(new URL(redirecturl, nextUrl))
+  if (!routeExists) {
+    return NextResponse.redirect(new URL('/404', nextUrl));
+  }
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isApiRoute && isLoggedIn) {
+    return NextResponse.next();
+  }
+
+  const role = cookies().get('role')?.value;
+  console.log(
+    nextUrl.pathname,
+    ':',
+    isUSER,
+    ' ',
+    isADMIN,
+    ' ',
+    isBANK,
+    ' ',
+    req.auth,
+    ' is login',
+    isLoggedIn,
+    '\nROLE: ',
+    role,
+  );
+
+  if (isAuthRoute || isPublicRoute) {
+    if (isLoggedIn) {
+      let redirecturl = DEFAULT_LOGIN_REDIRECT_USER;
+      if (role === 'USER') {
+        redirecturl = DEFAULT_LOGIN_REDIRECT_USER;
+      } else if (role === 'ADMIN') {
+        redirecturl = DEFAULT_LOGIN_REDIRECT_ADMIN;
+      } else if (role === 'BANK') {
+        redirecturl = DEFAULT_LOGIN_REDIRECT_BANK;
       }
-      return null
+
+      return NextResponse.redirect(new URL(redirecturl, nextUrl));
     }
-    if(isLoggedIn){
-      console.log(nextUrl.pathname,":",isUSER, " ",isADMIN, " ",isBANK," ",req.auth," ROLE:",role);
-      // role===BANK then they can not access routes in isADMIN AND isUSER
-      if ((isBANK || isADMIN)&& role==="USER") {
-          return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT_USER, nextUrl))
-      }else if((isUSER || isADMIN)&& role==="BANK"){
-          return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT_BANK, nextUrl))
-      }else if((isBANK || isUSER)&& role==="ADMIN"){
-          return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT_ADMIN, nextUrl))
-      }
-    }else{
-      return NextResponse.redirect(new URL("/auth/signin", nextUrl))
+    return null;
+  }
+  if (isLoggedIn) {
+    console.log(
+      nextUrl.pathname,
+      ':',
+      isUSER,
+      ' ',
+      isADMIN,
+      ' ',
+      isBANK,
+      ' ',
+      req.auth,
+      ' ROLE:',
+      role,
+    );
+    // role===BANK then they can not access routes in isADMIN AND isUSER
+    if ((isBANK || isADMIN) && role === 'USER') {
+      return NextResponse.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT_USER, nextUrl),
+      );
+    } else if ((isUSER || isADMIN) && role === 'BANK') {
+      return NextResponse.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT_BANK, nextUrl),
+      );
+    } else if ((isBANK || isUSER) && role === 'ADMIN') {
+      return NextResponse.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT_ADMIN, nextUrl),
+      );
     }
-  
-  return null
-})
+  } else {
+    return NextResponse.redirect(new URL('/auth/signin', nextUrl));
+  }
+
+  return null;
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next|404).*)", "/", "/(api|trpc)(.*)"],
-}
-
+  matcher: ['/((?!.*\\..*|_next|404).*)', '/', '/(api|trpc)(.*)'],
+};
