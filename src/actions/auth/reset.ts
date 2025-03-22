3aacb2423e58ba0a9f5483f66be8ce8248eb7a0c
sarthak mayani <mayanisarthak@gmail.com>
2025-03-22 07:24:37 +0530

    mail template image change

--- src/actions/auth/reset.ts
+++ src/actions/auth/reset.ts
@@ -126,7 +126,7 @@ const sendPasswordResetEmail = async(email: string, token: string, name: string)
     <div class="container">
         <div class="header">
             <div class="logo-container">
-                <img src="https://trackwithCreditunify.vercel.app/CreditUnify-logo.svg" alt="CreditUnify logo" class="logo">
+                <img src="https://drive.google.com/uc?export=view&id=1seDHzxeYDw0K8orLVrH0WVSHZP1mNH5v" alt="CreditUnify logo" class="logo">
                 <span class="logo-text">CreditUnify</span>
             </div>
         </div>
